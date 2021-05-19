b7178ac69af31d5edb005c1b6bb7bbc91d308368
Bong Nguyen <vanbong@gmail.com>
2021-05-19 18:20:11 +0800

    feat: parse package information

--- cmd/gogen/main.go
+++ cmd/gogen/main.go
@@ -9,6 +9,8 @@ var cli struct {
 	Name     string `kong:"arg,required,help='Name of a Go type'"`
 	Template string `kong:"required,type='existingfile',short='t',help='Path to the template file'"`
 	Output   string `kong:"help='Path to the output',short='o'"`
+	Dir      string `kong:"help='Directory to search for',default='.'"`
+	Format   bool   `kong:"short='f',help='Format source code'"`
 }
 
 func main() {
@@ -19,9 +21,11 @@ func main() {
 	)
 
 	g := &gogen.Generator{
+		Dir:          cli.Dir,
 		Name:         cli.Name,
 		TemplateFile: cli.Template,
 		Output:       cli.Output,
+		Format:       cli.Format,
 	}
 
 	ctx.FatalIfErrorf(g.Run())
--- gogen.go
+++ gogen.go
@@ -3,10 +3,7 @@ package gogen
 
 import (
 	"bytes"
-	"errors"
 	"go/ast"
-	"go/types"
-	"html/template"
 	"io"
 	"io/fs"
 	"io/ioutil"
@@ -31,14 +28,14 @@ var (
 
 // Description includes parsed information of a Go name. It will be used to feed data to the template.
 type Description struct {
-	Name    string
-	Pkg     Package
-	Methods []*Method
+	Name string
+	Pkg  *Package
 }
 
 // Generator is an execution to generate code. Call Run method to trigger the job.
 type Generator struct {
 	Dir          string
+	Format       bool
 	Name         string
 	Output       string
 	Template     string
@@ -48,24 +45,6 @@ type Generator struct {
 	buf  *bytes.Buffer
 	desc *Description
 	pkgs []*packages.Package
-}
-
-// Method presents a method.
-type Method struct {
-	Name    string
-	Params  []*Field
-	Results []*Field
-}
-
-// Field presents a field.
-type Field struct {
-	Name string
-	Type GoType
-}
-
-// GoType presents a Go type.
-type GoType struct {
-	types.Type
 }
 
 // Package presents a Go package.
@@ -125,25 +104,19 @@ func (g *Generator) extractDescription() error {
 							continue
 						}
 
-						sType, ok := spec.Type.(*ast.InterfaceType)
-						if !ok {
-							continue
-						}
-
 						if spec.Name.Name != g.Name {
 							continue
 						}
 
 						d := &Description{
 							Name: g.Name,
-							Pkg: Package{
+							Pkg: &Package{
 								Path: pkg.PkgPath,
 								Name: pkg.Name,
 							},
-							Methods: extractMethodsFromInterfaces(pkg, sType),
 						}
 
-						g.description = d
+						g.desc = d
 						return nil
 					}
 				}
@@ -151,7 +124,11 @@ func (g *Generator) extractDescription() error {
 		}
 	}
 
-	return errors.New("gogen: no Go type found")
+	g.desc = &Description{
+		Name: g.Name,
+	}
+
+	return nil
 }
 
 func (g *Generator) loadTemplate() error {
@@ -164,7 +141,6 @@ func (g *Generator) loadTemplate() error {
 		return err
 	}
 
-	g.template = string(templateContent)
 	g.Template = string(content)
 	return nil
 }
@@ -178,29 +154,21 @@ func (g *Generator) executeTemplate() error {
 	}
 
 	g.buf = new(bytes.Buffer)
-	return codeTmpl.Execute(g.buf, g.desc)
+	return compiledTempl.Execute(g.buf, g.desc)
 }
 
 func (g *Generator) formatSource() error {
-	out, err := imports.Process(g.OutFile, g.buf.Bytes(), nil)
+	if !g.Format {
+		return nil
+	}
+
+	out, err := imports.Process(g.Output, g.buf.Bytes(), nil)
 	if err != nil {
 		return err
 	}
 
 	g.buf = bytes.NewBuffer(out)
 	return nil
-}
-
-func (g *Generator) writeToFile() error {
-	f, err := os.Create(g.OutFile)
-	if err != nil {
-		return err
-	}
-
-	defer f.Close()
-
-	_, errWrite := g.buf.WriteTo(f)
-	return errWrite
 }
 
 func (g *Generator) writeToFile() error {
@@ -215,47 +183,4 @@ func (g *Generator) writeToFile() error {
 	}
 
 	return ioutil.WriteFile(g.Output, g.buf.Bytes(), fs.ModePerm)
-}
-
-func extractMethodsFromInterfaces(pkg *packages.Package, i *ast.InterfaceType) []*Method {
-	var methods []*Method
-	for _, method := range i.Methods.List {
-		fnDesl, ok := method.Type.(*ast.FuncType)
-		if !ok {
-			continue
-		}
-		fn := &Method{
-			Name:    method.Names[0].Name,
-			Params:  extractFieldsFromAst(pkg, fnDesl.Params.List),
-			Results: extractFieldsFromAst(pkg, fnDesl.Results.List),
-		}
-
-		methods = append(methods, fn)
-	}
-
-	return methods
-}
-
-func extractFieldsFromAst(pkg *packages.Package, items []*ast.Field) []*Field {
-	output := []*Field{}
-
-	for _, item := range items {
-		name := ""
-
-		//  nil if anonymous field
-		if len(item.Names) > 0 {
-			name = item.Names[0].Name
-		}
-
-		funcField := &Field{
-			Type: GoType{
-				Type: pkg.TypesInfo.TypeOf(item.Type),
-			},
-			Name: name,
-		}
-
-		output = append(output, funcField)
-	}
-
-	return output
 }
